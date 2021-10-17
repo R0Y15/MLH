67de63dfe4bca730263a98ea3e3dd3a44d418f68
ronnie8789 <bishalroy15jan@gmail.com>
2021-10-18 00:32:49 +0530

    mlh

--- game score/gamescore.py
+++ game score/gamescore.py
@@ -1,13 +1,42 @@
 #this is a simple code to save game score
+#this code is applicable to games with 2 players/teams only
+
 def score(ro):
-    sc_fin = 0
-    sc_lis = []
-    for i in range(ro):
-        sc = int(input("enter the current score to be saved:"))
-        sc_lis.append(sc)
-        sc_fin += sc
-    print ("the final score is:", sc_fin)
-    print ("the game scores per round were:", sc_lis)
+    sc_fin_1 = 0
+    sc_fin_2 = 0
+    sc_lis_1 = []
+    sc_lis_2 = []
+    for i in range(1, ro+1):
+        print("*" * 50)
+        print("Round", i, "is going on")
+
+        sc_1= int(input("enter the current score of team 1:"))
+        # saving the round scores for game 1
+
+        sc_2= int(input("enter the current score of team 2:"))
+        # saving the round scores for game 2
+
+        sc_lis_1.append(sc_1)
+        sc_lis_2.append(sc_2)
+        #adding up the scores to the list
+
+        sc_fin_1 += sc_1
+        sc_fin_2 += sc_2
+        #finalizing and adding up the scores
+
+    print("*" * 50)
+    print ("the final score of team 1 is:", sc_fin_1)
+    print ("the final score of team 2 is:", sc_fin_2)
+    print ("the game scores of team 1 were:", sc_lis_1)
+    print ("the game scores of team 2 were:", sc_lis_2)
+
+    #getting out the winner's name
+    if sc_fin_1 > sc_fin_2:
+        print ("The winner is team_1")
+    elif sc_fin_1 == sc_fin_2:
+        print("There's a TIE between the teams")
+    else:
+        print("The winner is team_2")
 
 ro = int(input("enter the number of rounds:"))
 score(ro)
